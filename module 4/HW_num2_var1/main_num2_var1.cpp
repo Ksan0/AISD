b42e1723c19a66c69177f87ab482486f086dc92c
Ksan0 <kgfq@mail.ru>
2013-12-17 20:01:38 +0300

    Update main_num2_var1.cpp

--- module 4/HW_num2_var1/main_num2_var1.cpp
+++ module 4/HW_num2_var1/main_num2_var1.cpp
@@ -6,9 +6,8 @@ using namespace std;
 typedef long long Sint64;
 
 
-Sint64 min_roads_dist(vector<Sint64> &coords)
-{
-    if(coords.empty() || coords.size() == 1)  // impossible to calculate
+Sint64 min_roads_dist(vector<Sint64> &coords) {
+    if (coords.empty() || coords.size() == 1)  // impossible to calculate
         return 0;
     
     sort(coords.begin(), coords.end());
@@ -17,8 +16,7 @@ Sint64 min_roads_dist(vector<Sint64> &coords)
     Sint64 a1 = a0;
     Sint64 a2 = 0;
 
-    for(unsigned int i = 1; i < coords.size() - 1 ; ++i)
-    {
+    for (unsigned int i = 1; i < coords.size() - 1 ; ++i) {
         a2 = coords[i+1] - coords[i] + min(a0, a1);
         
         // move left
@@ -31,12 +29,11 @@ Sint64 min_roads_dist(vector<Sint64> &coords)
 }
 
 
-int main()
-{
+int main() {
     int n;
     cin >> n;
     vector<Sint64> towns(n);
-    for(unsigned int i = 0; i < towns.size(); i++)
+    for (unsigned int i = 0; i < towns.size(); i++)
         cin >> towns[i];
 
     Sint64 res = min_roads_dist(towns);
