7cc143acbf89ea06175651d959752a4e6522f695
Ksan0 <kgfq@mail.ru>
2013-12-17 20:46:52 +0400

    Update

--- module 4/HW_num2_var1/main_num2_var1.cpp
+++ module 4/HW_num2_var1/main_num2_var1.cpp
@@ -3,8 +3,10 @@
 #include <algorithm>
 using namespace std;
 
+typedef long long Sint64;
 
-int min_roads_dist(vector<int> coords)
+
+Sint64 min_roads_dist(vector<Sint64> coords)
 {
     if(coords.empty() || coords.size() == 1)
         return 0;
@@ -12,12 +14,12 @@ int min_roads_dist(vector<int> coords)
         return coords[1] - coords[0];
 
     sort(coords.begin(), coords.end());
-    vector<int> road(coords.size(), 0);
+    vector<Sint64> road(coords.size(), 0);
 
 	road[0] = coords[1] - coords[0];
     road[1] = road[0];
 
-    for(int i = 1; i < coords.size() - 1 ; ++i)
+    for(unsigned int i = 1; i < coords.size() - 1 ; ++i)
 		road[i+1] = coords[i+1] - coords[i] + min(road[i-1], road[i]);
 
     return road.back();
@@ -28,13 +30,13 @@ int main()
 {
 	int n;
 	cin >> n;
-	vector<int> towns(n);
-    for( int i = 0; i < towns.size(); i++)
+	vector<Sint64> towns(n);
+    for(unsigned int i = 0; i < towns.size(); i++)
 		cin >> towns[i];
 
-    int res = min_roads_dist(towns);
+    Sint64 res = min_roads_dist(towns);
     cout << res;
 
-	//system("pause");
+	// cout << "\n\n"; system("pause");
 	return 0;
 }
